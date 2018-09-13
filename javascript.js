d11270b13eee786def2eb3b2e85df272a7647ce4
bencian <bencian94@gmail.com>
2018-09-13 17:57:07 -0300

    ahi esta andando, pero se abre en otra pestania. tenemos que abrir un issue asi saben que tenemos problemas con esto

--- javascript.js
+++ javascript.js
@@ -2,12 +2,18 @@ function login(){
 	nombreUsuario = document.getElementById("nombre").value;
 	contraUsuario = document.getElementById("contra").value;
 	if(nombreUsuario == "admin"){
-		alert("aca se redirige al menu de admin");
+		window.location.href = 'administracion.html';
+		//console.log(window.location);
+		//window.location = '/ProyectoSoftware/administracion.html';
+		//window.location.pathname = 'administracion.html'
+		//window.location.replace('http://www.google.com');
+		//window.location.href = 'D:/ProgrammingPrograms/xampp/htdocs/ProyectoSoftware/administracion.html';
+		//window.location = './administracion.html';
 	} else {
 		if(nombreUsuario == 'Jorge'){
 			alert("aca se redirige al menu normal");
 		} else {
-			alert("equivocado");
+			alert("Nombre de usuario incorrecto");
 		}
 	}
 }
