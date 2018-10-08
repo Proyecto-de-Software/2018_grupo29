f0945a3bcc5044dd433576c9c6ffa01924afa6c0
Fernando <fer.borgognoni@gmail.com>
2018-10-08 12:26:25 -0300

    Agregado algo de validacion con js y otras cosas

---
+++ templates/js/login.js
@@ -0,0 +1,13 @@
+function validarLogin() {
+	var a = document.forms["login"]["nombre"].value;
+   var b = document.forms["login"]["contra"].value;
+   
+    if (b.length < 6) {
+    	 document.getElementById("campo_error").innerHTML = "La contraseña tiene menos de 6 caracteres";
+        return false;
+    }
+    if (a.length < 6) {
+       document.getElementById("campo_error").innerHTML = "El nombre de usuario tiene menos de 6 caracteres";
+        return false;
+    }
+}
